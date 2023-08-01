5dd9f1b6630b7a3870eb8f14feab87748094f0e9
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-08-01 20:37:22 +0500

    added packet in settings

--- workout_api/settings.py
+++ workout_api/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+
     'user_check',
+    'user_services',
 ]
 
 MIDDLEWARE = [
