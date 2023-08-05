14156d8336db411928a040aa3c74e60fe3219052
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-08-06 00:11:12 +0500

    added teacher view

--- user_services/views.py
+++ user_services/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Teacher
+from .serializers import TeacherSerializer
+from .permissions import IsTeacherOrAdmin
+
+class TeacherListCreateView(generics.ListCreateAPIView):
+    queryset=Teacher.objects.all()
+    serializer_class=TeacherSerializer
+    permission_classes=[IsAuthenticated,IsTeacherOrAdmin]
