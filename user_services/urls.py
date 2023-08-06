42dc4e58b88aeb79d3fc222606b53cd488ead470
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-08-06 15:55:57 +0500

    changed

--- user_services/urls.py
+++ user_services/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import TeacherListCreateView,TeacherDetailView
+from .views import TeacherListCreateView,TeacherDetailView,NormalUserListCreateView,NormalUserDetailView
 
 
 
 urlpatterns=[
     path("teacher/",TeacherListCreateView.as_view()),
+    path("learner/",NormalUserListCreateView.as_view()),
 ]
--- user_services/views.py
+++ user_services/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Teacher
-from .serializers import TeacherSerializer
-from .permissions import IsTeacherOrAdmin
+from .models import Teacher,NormalUser
+from .serializers import TeacherSerializer,NormalUserSerializer
+from .permissions import IsTeacherOrAdmin,IsNormalUser
 
 class TeacherListCreateView(generics.ListCreateAPIView):
     queryset=Teacher.objects.all()
@@ -17,3 +17,15 @@ class TeacherDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset=Teacher.objects.all()
     serializer_class=TeacherSerializer
     permission_classes=[IsAuthenticated,IsTeacherOrAdmin]
+
+
+class NormalUserListCreateView(generics.ListCreateAPIView):
+    queryset=NormalUser.objects.all()
+    serializer_class=NormalUserSerializer
+    permission_classes=[IsAuthenticated,IsNormalUser]
+
+
+class NormalUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=NormalUser.objects.all()
+    serializer_class=NormalUserSerializer
+    permission_classes=[IsAuthenticated,IsNormalUser]
