5d0f89ba50cce2e07a89f7d5547704ec42f1dd73
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-08-21 20:54:11 +0500

    commit

--- user_services/models.py
+++ user_services/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 # from django.contrib.auth.models import User
 
-from django.conf import settings
+from django.conf import settings 
 
 
 class CustomUserManager(BaseUserManager):
