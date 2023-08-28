60c38d12d889a170858066d2523b2bf9facccdfa
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-08-28 14:52:08 +0500

    changed

--- user_services/admin.py
+++ user_services/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Teacher,NormalUser
+# from .models import Teacher,NormalUser
 
-admin.site.register(Teacher)
-admin.site.register(NormalUser)
+# admin.site.register(Teacher)
+# admin.site.register(NormalUser)
