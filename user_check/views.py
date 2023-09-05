ddd1ee06d8b9fc3bf709fc6321bb8133f8b88458
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-09-05 19:35:50 +0500

    commit CreateUserView

--- user_check/views.py
+++ user_check/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 # from .serializers import MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import SignUPSerializer
+from rest_framework import permissions,status
+from rest_framework.generics import CreateAPIView
 
 # class LoginView(TokenObtainPairView):
 #     serializer_class=MyTokenObtainPairSerializer
+
+
+
+class CreateUserview(CreateAPIView):
+    model = User
+    permission_classes=(permissions.AllowAny,)
+    serializer_class=SignUPSerializer
